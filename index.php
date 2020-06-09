2096f02c60733ea1cc3e372c9efe515f927cc44b
jerzhy <grenngreen80@gmail.com>
2020-06-09 09:29:33 +0300

    fixed

--- index.php
+++ index.php
@@ -1,24 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>Document</title>
+	<title>Bootstrap Eto</title>
 	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="viewport" content="width=device-width, shrink-to-fit=no">
 	<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-	<link rel="stylesheet" href="css/main .css">
+	<link rel="stylesheet" href="css/main.css">
 </head>
 <body>
-	<div class="navbar navbar-inverse navbar-fixed-top"> 
-	</div>
+	<header>
+		
+	</header>
+
+
+
 	<div class="contanier">
-		<div class="navbar-header">
-			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-				<span class="icon-bar">q</span>	
-				<span class="icon-bar">q</span>	
-				<span class="icon-bar">q</span>	
-			</button>
-		</div>
+	    MyFirstSleep
+		
 	</div>
-	MyFirstSleep
+	<footer></footer>
 </body>
-</html>
+</html> 
