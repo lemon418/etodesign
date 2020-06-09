47b2b6e20a129a207842aefde4080b955e228810
jerzhy <grenngreen80@gmail.com>
2020-06-09 08:24:08 +0300

    init

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Document</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/main .css">
+</head>
+<body>
+	<div class="navbar navbar-inverse navbar-fixed-top"> 
+	</div>
+	<div class="contanier">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+				<span class="icon-bar">q</span>	
+				<span class="icon-bar">q</span>	
+				<span class="icon-bar">q</span>	
+			</button>
+		</div>
+	</div>
+	MyFirstSleep
+</body>
+</html>
