d0cc2bd656fdadd0577a1a24460ed62cd3654406
ogneboy <pavel@eto-design.ru>
2020-06-11 17:41:59 +0300

    LEFT JOIN3

--- registration.php
+++ registration.php
@@ -15,7 +15,7 @@
             <input type="text" class="form-control" name="name" id="name" placeholder="Введите имя"><br>
             <input type="text" class="form-control" name="login" id="login" placeholder="Введите логин"><br>
             <input type="text" class="form-control" name="psw" id="psw" placeholder="Введите пароль"><br>
-            <button class="btn btn-success" type="submit">Зарегистрировать</button>
+            <button class="btn btn-success" type="submit">Зарегистрироваь</button>
         </form>
     </div>
 </body>
